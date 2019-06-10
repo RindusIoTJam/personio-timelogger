6d46c1ebbc147ad2bcb42932fc7e9d4fbe7d1896
Manfred Ackermann <m.ackermann@douglas.de>
2019-06-10 09:46:40 +0200

    Changed the calls to personio to be done in Python3.

--- client/personio-timelogger.py
+++ client/personio-timelogger.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
-from config_sample import *
-from config import *
 import sys
 import os
 import json
 import re
+import urllib.request as request
+import urllib.parse as parse
+import http.cookiejar
+
 from random import randint
 from datetime import timedelta
+
+from config_sample import *
+try:
+	from config import *
+except ImportError:
+	print('WARNING: no config.py found. Please RTFM!')
+	sys.exit(1)
 
 if len(sys.argv) == 1 or sys.argv[1] == '--help':
 	help_message = 'Error. No argument' + '\n\n'
@@ -16,10 +25,12 @@ if len(sys.argv) == 1 or sys.argv[1] == '--help':
 	print(help_message)
 	exit()
 
-def checkDate (dateInput):
+
+def checkDate(dateInput):
 	return re.fullmatch(r"\A([\d]{4})-([\d]{2})-([\d]{2})", dateInput)
 
-if (not(checkDate(sys.argv[1]))):
+
+if not checkDate(sys.argv[1]):
 	error_message = 'Error. Wrong date format' + '\n\n'
 	error_message += 'Usage: att.py [date]' + '\n'
 	error_message += 'Note: Date format yyyy/mm/dd' + '\n'
@@ -29,51 +40,9 @@ if (not(checkDate(sys.argv[1]))):
 WORKING_TIME = WORKING_HOURS * 60
 MIN_START_TIME = STARTING_HOUR * 60
 COOKIE = 'cookie.txt'
-LOGIN_URL = 'https://rindus.personio.de/login/index';
-ATTENDANCE_URL = 'https://rindus.personio.de/api/v1/employees/' + PROFILE_ID+ '/attendances';
+LOGIN_URL = 'https://rindus.personio.de/login/index'
+ATTENDANCE_URL = 'https://rindus.personio.de/api/v1/employees/' + PROFILE_ID + '/attendances'
 
-def clearCookie():
-	with open(COOKIE, "w"):
-		pass
-
-def doGet(url):
-	command = ('curl ' + url +
-		' --insecure' + 
-		' --location' +
-		' --cookie ' + COOKIE +
-		' --cookie-jar ' + COOKIE +
-		' >/dev/null 2>&1'
-	)
-	os.system(command)
-	return
-
-def doPost(url, data):
-	command = ('curl ' + url +
-		# ' -X POST' +
-		' --insecure' + 
-		' --location' +
-		' --cookie ' + COOKIE +
-		' --cookie-jar ' + COOKIE +
-		' --data \'' + data + '\'' +
-		' >/dev/null 2>&1'
-	)
-	os.system(command)
-	return
-
-
-def doPostJSON(url, data):
-	json_data = json.dumps(data)
-	command = ('curl ' + url +
-		' -X POST' +
-		' --header "Content-Type: application/json"' +
-		' --header "Content-Length: ' + str(len(json_data)) + '"' +
-		' --insecure' + 
-		' --cookie ' + COOKIE +
-		' --cookie-jar ' + COOKIE +
-		' --data \'' + json_data + '\''
-	)
-	os.system(command)
-	return
 
 def generateAttendance(date):
 	post_date = date + 'T00:00:00+02:00'
@@ -83,6 +52,7 @@ def generateAttendance(date):
 	end_time = start_time + working_time
 
 	return [{
+		'email': EMAIL,
 		'id': None,
 		'date': post_date,
 		'start_time': start_time,
@@ -91,12 +61,6 @@ def generateAttendance(date):
 		'comment': ''
 	}]
 
-def objectToURLParams(obj):
-	output = ''
-	for name, value in obj.items():
-		output = output + name + '=' + value + '&'
-
-	return output
 
 def slack_bang(data):
 	postData = {
@@ -110,21 +74,33 @@ def slack_bang(data):
 
 	json_data = json.dumps(postData)
 
-	
 	command = "curl --silent -X POST -H 'Content-type: application/json'"
-	command += " --data '" +json_data + "'"
+	command += " --data '" + json_data + "'"
 	command += " " + SLACK_BOT_URL + "/timelogger"
 	command += " > /dev/null"
-	#print(command)
+	# print(command)
 	os.system(command)
 
-# RUN
-clearCookie()
-doGet(LOGIN_URL)
-login_data = 'email=' + EMAIL + '&password=' + PASSWORD
-doPost(LOGIN_URL, login_data)
 
-attendance_entry = generateAttendance(sys.argv[1])
-#print(attendance_data)
-doPostJSON(ATTENDANCE_URL, attendance_entry)
-slack_bang(attendance_entry)
+if __name__ == "__main__":
+	# RUN
+
+	cookieJar = http.cookiejar.CookieJar()
+	cookiePro = request.HTTPCookieProcessor(cookieJar)
+	urlOpener = request.build_opener(cookiePro)
+
+	# Login
+	login_data = {
+		'email':    EMAIL,
+		'password': PASSWORD
+	}
+	data = parse.urlencode(login_data).encode()
+	#urlOpener.open(LOGIN_URL, data=data)
+
+	# Add attendance
+	attendance_entry = generateAttendance(sys.argv[1])
+	#urlOpener.addheaders = [('Content-Type', 'application/json')]
+	#urlOpener.open(ATTENDANCE_URL, data=bytes(json.dumps(attendance_entry), 'utf-8'))
+
+	# Inform User
+	slack_bang(attendance_entry)
