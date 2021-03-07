7b75b33684535a13b4925f14a8eebf6e45396bf9
David Lopez <davilopez@gmail.com>
2021-03-08 00:51:04 +0100

    feat: indicate teh hour to take the break

--- client/config_sample.py
+++ client/config_sample.py
@@ -10,9 +10,9 @@ SLACK_BOT_URL = 'URL-TO-BOT'
 #App configuration
 STARTING_HOUR = 7 # Hour you usually start working in the morning
 WORKING_HOURS = 9 # Including break time
+BREAK_HOUR = 13
 
 # Personio Configuration
 COOKIE = "cookie.txt"
 LOGIN_URL = "https://rindus.personio.de/login/index"
 ATTENDANCE_URL = f'https://rindus.personio.de/api/v1/attendances/periods'
-WORKING_DAYS = ["None", "Home Office", "Ninguno"]
