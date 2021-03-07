13aa0d751590de17a68f039ddadf06a170bca89b
David Lopez <davilopez@gmail.com>
2021-03-08 00:50:36 +0100

    feat: create the new attendance format

--- client/personio-timelogger.py
+++ client/personio-timelogger.py
@@ -4,11 +4,9 @@ import sys
 import os
 import json
 import re
-import urllib.request as request
-import urllib.parse as parse
-import http.cookiejar
 import pytz
 import requests
+import uuid
 
 from time import sleep
 from random import randint
@@ -16,69 +14,42 @@ from datetime import timedelta
 from datetime import datetime
 
 try:
-    from config import (
+    from config_sample import (
         ATTENDANCE_URL,
+        BREAK_HOUR,
         EMAIL,
         LOGIN_URL,
         PASSWORD,
+        PROFILE_ID,
         SLACK_BOT_URL,
+        SLACK_MESSAGE,
         SLACK_SECRET,
         STARTING_HOUR,
-        WORKING_DAYS,
-        WORKING_HOURS
+        WORKING_HOURS,
     )
 
 except ImportError:
     print("WARNING: no config.py found. Please RTFM!")
     sys.exit(1)
 
-if len(sys.argv) == 1 or sys.argv[1] == "--help":
-    help_message = "Error. No argument" + "\n\n"
-    help_message += "Usage: %s [date]" % __file__ + "\n"
-    help_message += "Note: Date format yyyy-mm-dd" + "\n"
-    print(help_message)
-    exit()
 
-WORKING_TIME = WORKING_HOURS * 60
-MIN_START_TIME = STARTING_HOUR * 60
-
-def is_dst(dt=None, timezone="UTC"):
-    if dt is None:
-        dt = datetime.utcnow()
-    timezone = pytz.timezone(timezone)
-    timezone_aware_date = timezone.localize(dt, is_dst=None)
-    return timezone_aware_date.tzinfo._dst.seconds != 0
-
-
-def checkDate(dateInput):
+def check_date(dateInput):
     return re.fullmatch(r"\A([\d]{4})-([\d]{2})-([\d]{2})", dateInput)
 
-if not checkDate(sys.argv[1]):
-    error_message = "Error. Wrong date format" + "\n\n"
-    error_message += "Usage: %s [date]" % __file__ + "\n"
-    error_message += "Note: Date format yyyy-mm-dd" + "\n"
-    print(error_message)
-    exit()
-
-def formatDate(date):
-    if is_dst(timezone="Europe/Madrid"):
-        return date + "T00:00:00+02:00"
-    else:
-        return date + "T00:00:00+01:00"
-
-
-def generateMessage(date, message):
-    post_date = formatDate(date)
-    return [{"email": EMAIL, "date": post_date, "message": message}]
 
 def format_datetime(date, time):
-    return f'{date}T{time}Z'
-    
-def generateAttendance(date):
-    post_date = formatDate(date)
-    break_time = randint(50, 70)
-    start_time = MIN_START_TIME + randint(5, 45)
-    working_time = randint(WORKING_TIME - 25, WORKING_TIME - 5)
+    return f"{date}T{time}Z"
+
+
+def generate_attendance(date, starting_hour, break_hour, working_hours, employee_id):
+    min_start_time = starting_hour * 60
+    working_time = working_hours * 60
+    break_time = break_hour * 60
+
+    # randint(50, 70)
+
+    start_time = min_start_time + randint(5, 45)
+    working_time = randint(working_time - 25, working_time - 5)
     end_time = start_time + working_time
 
     return [
@@ -99,20 +70,8 @@ def generateAttendance(date):
             "project_id": None,
             "employee_id": employee_id,
             "activity_id": None,
-        }
+        },
     ]
-
-
-def getDayInfo(date, urlOpener):
-    month = date[:7]
-    post_date = formatDate(date)
-    response = urlOpener.open(ATTENDANCE_URL + "/" + month).read().decode("utf-8")
-    day_list = json.loads(response)["data"]["rows"]
-    the_day = next((x for x in day_list if x["date"] == post_date), False)
-    if the_day:
-        if the_day["absences_holidays"] in WORKING_DAYS:
-            return {"isWorkingDay": True}
-    return {"isWorkingDay": False, "dayLabel": the_day["absences_holidays"]}
 
 
 def slack_bang(data):
@@ -145,34 +104,48 @@ def slack_bang(data):
 
 
 if __name__ == "__main__":
-    # RUN
+    if len(sys.argv) == 1 or sys.argv[1] == "--help" or not check_date(sys.argv[1]):
+        print(
+            f"""
+            Error. No argument or wrong date format\n\n
+            Usage: {__file__} [date]\n
+            Note: Date format yyyy-mm-dd \n
+            """
+        )
+        exit()
+
+    attendance_date = sys.argv[1]
 
     # Wait between 1-10 seconds before logging in
     sleep(randint(1, 10))
 
-    # Create request session
-    session = requests.Session()
+    print(generate_attendance(
+        sys.argv[1], STARTING_HOUR, BREAK_HOUR, WORKING_HOURS, PROFILE_ID
+    ))
 
-    response = session.post(
-        LOGIN_URL,
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-        data={"email": EMAIL, "password": PASSWORD}
-    )
+    # # Create request session
+    # session = requests.Session()
 
+    # # Login into Personio
+    # response = session.post(
+    #     LOGIN_URL,
+    #     headers={"Content-Type": "application/x-www-form-urlencoded"},
+    #     data={"email": EMAIL, "password": PASSWORD},
+    # )
 
+    # # Log the attendance
+    # response = session.post(
+    #     ATTENDANCE_URL,
+    #     json=generate_attendance(
+    #         sys.argv[1], STARTING_HOUR, BREAK_HOUR, WORKING_HOURS, PROFILE_ID
+    #     ),
+    # )
 
-    response = session.post(
-        ATTENDANCE_URL,
-        json=[format_period_data(
-            attendance_date, "09:31:00", "16:45:00", PROFILE_ID
-        )],
-    )
-
-    data = json.loads(response.text)
-    try:
-        return f"Error: {data['error']['message']}"
-    except KeyError:
-        return f"Success: attendance on {attendance_date} registered!"
+    # data = json.loads(response.text)
+    # try:
+    #     message = f"Error: {attendance_date} - {data['error']['message']}"
+    # except KeyError:
+    #     message = f"Success: attendance on {attendance_date} registered!"
 
     # Inform User
-    slack_bang(response.text)
+    # slack_bang(message)
